f4ff2cea2834bbb337b86b486ca24598391c9dcb
lnxricardo <solucionartec@gmail.com>
2020-07-16 16:04:30 -0300

    teste

---
+++ index.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SOURCE="/tmp/clean-disk"
+FILE="limpar-disco.sh"
+DESTINY="/scripts/"
+
+cd $DESTINY
+sleep 2
+if [ ! -f $FILE ]
+then
+
+sleep 2
+cp /$SOURCE/$FILE $DESTINY
+chmod +x ./$FILE
+else
+    cat < $SOURCE/$FILE > $FILE
+fi
